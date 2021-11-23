5df612cdd263e62e1d1ed92aa87517b0fd813afa
emproy91 <emproy91@gmail.com>
2021-11-23 10:37:54 -0500

    primera grafica seccion 6 1warning

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { SharedModule } from '../shared/shared.module';
 import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
+import { ChartsModule } from 'ng2-charts';
+/*Traido de : valor-software.com/ng2-charts/#/GeneralInfo . para usar en global poner en app module .
+*/
+// Modulos hechos
+import { SharedModule } from '../shared/shared.module';
+import { ComponentsModule } from '../components/components.module';
+// modulos hechos.
 
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProgressComponent } from './progress/progress.component';
@@ -23,8 +31,12 @@ import { PagesComponent } from './pages.component';
   ],
   imports: [
     CommonModule,
-    SharedModule, 
-    RouterModule
+    FormsModule,
+    SharedModule,
+    ChartsModule,
+    RouterModule,
+    ComponentsModule
+    
   ]
 })
 export class PagesModule { }
